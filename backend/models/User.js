c8f551c0f6bc7ad55914763cdddc63cebdcdd2d9
TheZaxxx <159675281+TheZaxxx@users.noreply.github.com>
2025-11-15 08:36:01 +0700

    Add files via upload

---
+++ backend/models/User.js
@@ -0,0 +1,23 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config/db.js";
+
+export const User = sequelize.define(
+  "users",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  },
+  { timestamps: true }
+);
